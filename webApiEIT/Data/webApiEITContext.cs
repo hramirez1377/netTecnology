f282a80c2546a0dfa4aba9b2236e483d6a8ccc8a
Hernan Ramirez <hramirez1377@gmail.com>
2021-09-23 21:33:43 -0500

    Add project files.

---
+++ webApiEIT/Data/webApiEITContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webApiEIT.Models;
+
+namespace webApiEIT.Data
+{
+    public class webApiEITContext : DbContext
+    {
+        public webApiEITContext (DbContextOptions<webApiEITContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webApiEIT.Models.Book> Book { get; set; }
+    }
+}
